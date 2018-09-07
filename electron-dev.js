515615640dfd1bd284c260f89c0223f435a0284c
Andy <admazzola@gmail.com>
2018-09-06 23:20:15 -0400

    fix bug in electron dev

--- electron-dev.js
+++ electron-dev.js
@@ -1,8 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 
-const EditorCore = require('./lib/editorcore')
-const EditorCore = require('./lib/editorcore')
+const EditorCore = require('./lib/editorcore') 
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
